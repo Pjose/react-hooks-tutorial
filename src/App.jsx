48406327420eed26e2511b59efe64d3b83a5fb80
Peter Mwangi <pjmwangi@hotmail.com>
2024-08-30 20:49:32 -0500

    Add useState example

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
+import { useState } from "react"
+
 const App = () => {
+  const [count, setCount] = useState(0)
+
   return (
-    <div>App</div>
+    <div>
+      <h1>useState Explained!</h1>
+      <h3>Count: {count}</h3>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
   )
 }
 
