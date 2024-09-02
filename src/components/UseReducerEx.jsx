d98ca655162e181513792dab4612ed1d3efc3a4b
Peter Mwangi <pjmwangi@hotmail.com>
2024-09-01 22:55:51 -0500

    Edit useReducer to add error state

--- src/components/UseReducerEx.jsx
+++ src/components/UseReducerEx.jsx
@@ -1,13 +1,28 @@
 import { useReducer } from 'react'
 
-const initialState = { count: 0 };
+const initialState = {
+    count: 0,
+    error: null,
+};
 
 function reducer(state, action) {
   switch (action.type) {
-    case 'INCREMENT':
-      return { count: state.count + 1 };
-    case 'DECREMENT':
-      return { count: state.count - 1 };
+      case 'INCREMENT': {
+          const newCount = state.count + 1;
+          const hasError = newCount > 5;
+          return {
+              count: hasError ? state.count : newCount,
+              error: hasError ? 'Maximum reached' : null,
+          };
+      }
+    case 'DECREMENT': {
+        const newCount = state.count - 1;
+        const hasError = newCount < 0;
+        return {
+            count: hasError ? state.count : newCount,
+            error: hasError ? 'Minimum reached' : null,
+        };
+    }
     default:
       throw new Error();
   }
@@ -21,6 +36,7 @@ const UseReducerEx = () => {
             <button onClick={() => dispatch({ type: 'DECREMENT' })}>-</button>
             Count: {state.count}
             <button onClick={() => dispatch({ type: 'INCREMENT' })}>+</button>
+            {state.error && <div className='mt-2 text-red-600'>{state.error}</div>}
         </div>
   )
 }
